17f32e448873bc195abd36f46eb92f2e82b2781a
Ahmad Yuridan <mamadsander@gmail.com>
2023-08-06 14:38:35 +0700

    first commit

---
+++ src/test/java/com/rental/Server/Eureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.Server.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
